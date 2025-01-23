30ed0cb9e62a72f2d043fea56d3e9c38761a045e
kelseyzimmerman <95315048+kelseyzimmerman@users.noreply.github.com>
2025-01-23 12:14:04 -0600

    exercise1

---
+++ week 1/exercise 1/hello-world.js
@@ -0,0 +1,5 @@
+// print Hello World to console
+console.log("Hello, world")
+// ask for username and define username as variable
+let userName = window.prompt("What is your name?");
+console.log("Hello, " + userName)
