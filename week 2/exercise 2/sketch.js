0272d6e4e5bf9aca2446a4cc432d5abb24077f82
kelseyzimmerman <95315048+kelseyzimmerman@users.noreply.github.com>
2025-01-30 18:14:48 -0600

    exercise 2

--- week 2/exercise 2/sketch.js
+++ week 2/exercise 2/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-}
+// create an array with my favorite games as objects inside array
+const faveGames = [
+  {game: "Monopoly",
+    type: "board",
+    numberPlayers: "2 +",
+    shortDesc: "the evil banker game",
+  },
+  {game: "Head's Up Seven Up",
+    type: "childhood classroom",
+    numberPlayers: "3+",
+    shortDesc: "how to tell who likes whom, elementary-school style",
+  },
+  {game: "Tabboo",
+    type: "party",
+    numberPlayers: "4+",
+    shortDesc: "pairs of people testing how well they know one another"
+  }
+]
+const whichGame = window.prompt("I have three games in my collection! Which shall we play? Select a number between 1 and 3.");
+//if(whichGame === faveGames[1].game);
+window.alert("You have selected " + faveGames[whichGame-1].game + ", a " + faveGames[whichGame-1].type + " game for " + faveGames[whichGame-1].numberPlayers + " players" + "! This game can be described as: " + faveGames[whichGame-1].shortDesc + ".")
